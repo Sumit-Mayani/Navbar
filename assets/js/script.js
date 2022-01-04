4eba629645e708a60377b85151727b11a98212f5
Sumit Mayani <sumi.mayani@gmail.com>
2022-01-04 17:46:44 +0530

    Sumit Mayani

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
-(function ($) {
+window.onload = function () {
     "use strict";
 
-    
+    const menu_btn = document.querySelector('.main__header .humburger');
+    const mobile_nav = document.querySelector('.mobile__nav');
 
-})(jQuery);
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_nav.classList.toggle('is-active');
+    })
+
+};
